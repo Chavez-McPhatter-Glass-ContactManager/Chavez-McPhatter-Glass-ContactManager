df44237f3a7c0fe06cd6f980ce4f4205527ce8ed
Raymond Glass <raymond.allen.glass@gmail.com>
2023-06-01 16:46:19 -0500

    added switch statement

--- src/ContactManagerApp.java
+++ src/ContactManagerApp.java
@@ -1,33 +1,53 @@
-import utils.Input;
-import java.awt.*;
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.Scanner;
 
 public class ContactManagerApp {
 
     public static void main(String[] args) {
-//        menuList();
-//        Input userInput = new Input();
-//        userInput.getString();
 
-        info.add("two");
-        info.add("three");
-        contactList(info);
-        Path myFile = Paths.get("src", "contacts.txt");
+        boolean exitRequested = false;
+        while(!exitRequested) {
+            listMenu(); // need to incorporate scanner then add functionality through switch statements
+            int options = Integer.parseInt(sc.nextLine());
+            switch (options) {
+                case 1:
+                    displayContacts(); // reads data from contacts.txt
+                    break;
+                case 2:
+                    addContact(); // takes input from scanner and adds to contacts.txt
+                    break;
+                case 3:
+                    searchContact(); // searches contact from contacts.txt
+                    break;
+                case 4:
+                    deleteContact(); // deletes contact from contacts.txt
+                    break;
+                case 5:
+                    exitApplication(); = true; // boolean prompt to exit program?
+                    break;
+            }
+        }
+    }
+
+        Input userInput = new Input();
+        userInput.getString();
+//
+//        info.add("two");
+//        info.add("three");
+//        contactList(info);
+//        Path myFile = Paths.get("src", "contacts.txt");
 //        System.out.println(Files.exists(myFile));
 //        List<String> contact = Arrays.asList("Joe", "John", "Joseph");
 //        try {
 //            Files.write(myFile, contact);
 //        }
 
-    }
-
-    public static void menuList() {
+    //    ------- FUNCTIONS -------
+    public static void listMenu() {
         System.out.println("1. View Contacts.");
         System.out.println("2. Add a New Contact.");
         System.out.println("3. Search a Contact by Name.");
@@ -36,7 +56,10 @@ public class ContactManagerApp {
         System.out.println("Enter an option (1, 2, 3, 4, or 5)");
     }
 
-    public static Hashtable contactList(ArrayList<String> info) {
+    private static void displayContacts() {
+    }
+
+    public static void addContact() {
         Hashtable<String, Contact> information = new Hashtable<>();
         try {
             Path test = Paths.get("contacts.txt");
@@ -46,6 +69,15 @@ public class ContactManagerApp {
         }catch (Exception ie){
             ie.printStackTrace();
         }
-            return information;
     }
+    private static void searchContact() {
+    }
+
+    private static void deleteContact() {
+    }
+
+    private static void exitApplication() {
+    }
+
+
 }
